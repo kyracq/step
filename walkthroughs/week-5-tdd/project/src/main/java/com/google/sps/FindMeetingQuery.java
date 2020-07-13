ad6232609a7db95dd45bbfc23b360fe64dd05c3d
kyracq <kyraa@google.com>
2020-07-13 15:13:11 -0400

    Inital commit, tests 1-3

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,57 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public final class FindMeetingQuery {
+  /**
+   * Given the currently booked events (time ranges and attendees) and
+   * a meeting request (duration and attendees), return a collection
+   * the of time ranges within which the meeting can be booked.
+   */
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Collection<TimeRange> results = new ArrayList<TimeRange>();
+    Collection<String> attendees = request.getAttendees();
+    long duration = request.getDuration();
+
+    if (duration > TimeRange.WHOLE_DAY.duration()) {
+      return results;
+    }
+    if (attendees.isEmpty()) {
+      results.add(TimeRange.WHOLE_DAY);
+      return results;
+    }
+
+    ArrayList<TimeRange> invalidRanges = new ArrayList<TimeRange>();
+    for (Event event : events) {
+      Set<String> intersection = new HashSet<String>(attendees);
+      
+      if(!Collections.disjoint(attendees, event.getAttendees())) {
+        // Requested meeting can't be at same time as event
+        invalidRanges.add(event.getWhen());
+      }
+    }
+    Collections.sort(invalidRanges, TimeRange.ORDER_BY_START);
+    int validRangeStart;
+    // First time range
+    if(invalidRanges.get(0).start() != TimeRange.START_OF_DAY) {
+      validRangeStart = TimeRange.START_OF_DAY;
+    } else {
+      validRangeStart = invalidRanges.get(0).start();
+    }
+    int validRangeEnd;
+    for (TimeRange range : invalidRanges) {
+      validRangeEnd = range.start();
+      results.add(TimeRange.fromStartEnd(validRangeStart, validRangeEnd, false));
+    }
+    validRangeEnd = invalidRanges.get(invalidRanges.size() - 1).end(); 
+    results.add(TimeRange.fromStartEnd(validRangeEnd, TimeRange.END_OF_DAY, true));
+    return results;
   }
 }
